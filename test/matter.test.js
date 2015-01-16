49145b2b1b4c680a085dfebac7d597d3b7bc564f
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-16 08:56:31 -0500

    add tests for `.test()` method

---
+++ test/matter.test.js
@@ -0,0 +1,21 @@
+/*!
+ * gray-matter <https://github.com/assemble/gray-matter>
+ *
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+var should = require('should');
+var matter = require('..');
+
+describe('Read from strings:', function () {
+  it('should return `true` if the string has front-matter:', function () {
+    matter.test('---\nabc: xyz\n---').should.be.true;
+    matter.test('---\nabc: xyz\n---', {delim: '~~~'}).should.be.false;
+    matter.test('~~~\nabc: xyz\n~~~', {delim: '~~~'}).should.be.true;
+    matter.test('\nabc: xyz\n---').should.be.false;
+  });
+});
