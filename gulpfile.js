efd1ff4a388e8b0a3c25952355e57b4a52969ac5
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-16 20:16:58 -0400

    update js-yaml version

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
 var istanbul = require('gulp-istanbul');
+var uglify = require('gulp-uglify');
 var mocha = require('gulp-mocha');
 
-gulp.task('default', function(cb) {
-  gulp.src(['index.js', 'lib/*.js'])
+gulp.task('coverage', function(cb) {
+  gulp.src(['index.js', 'lib/parsers.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', function() {
@@ -15,3 +17,15 @@ gulp.task('default', function(cb) {
         .on('end', cb);
     });
 });
+
+gulp.task('uglify', function(cb) {
+  var base = path.dirname(require.resolve('js-yaml'));
+  gulp.src(path.resolve(base, 'dist/js-yaml.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('lib/'))
+    .on('end', cb)
+});
+
+gulp.task('default', ['coverage', 'uglify']);
+
+
