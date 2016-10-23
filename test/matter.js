e7f178eb41662ea0d448e7393915ee8efefef3ae
Pawel Kadluczka <moozzyk@gmail.com>
2016-10-22 22:58:38 -0700

    Removing string interpolation to fix tests on older node

--- test/matter.js
+++ test/matter.js
@@ -9,9 +9,9 @@ var lineEndings = ['\n', '\r\n'];
 
 lineEndings.forEach(function(lineEnding) {
 
-describe(`Read from strings with lineEnding ${lineEnding.replace('\n', '\\n').replace('\r', '\\r')}:`, function () {
+describe('Read from strings with lineEnding ' + lineEnding.replace('\n', '\\n').replace('\r', '\\r') + ':', function () {
   it('should extract YAML front matter', function () {
-    var actual = matter(`---${lineEnding}abc: xyz${lineEnding}---`);
+    var actual = matter('---' + lineEnding + 'abc: xyz' + lineEnding + '---');
     actual.should.have.property('data');
     actual.should.have.property('content');
     actual.should.have.property('orig');
@@ -21,8 +21,8 @@ describe(`Read from strings with lineEnding ${lineEnding.replace('\n', '\\n').re
 
   it('should throw an error when front-matter cannot be parsed:', function () {
     (function() {
-      matter(`---whatever${lineEnding}abc: xyz${lineEnding}---`)
-    }).should.throw(`gray-matter cannot find a parser for: ---whatever${lineEnding}abc: xyz${lineEnding}---`);
+      matter('---whatever' + lineEnding + 'abc: xyz' + lineEnding + '---')
+    }).should.throw('gray-matter cannot find a parser for: ---whatever' + lineEnding + 'abc: xyz' + lineEnding + '---');
   });
 
   it('should throw an error when a string is not passed:', function () {
@@ -36,7 +36,7 @@ describe(`Read from strings with lineEnding ${lineEnding.replace('\n', '\\n').re
   });
 
   it('should extract YAML front matter and content', function () {
-    var fixture = `---${lineEnding}abc: xyz${lineEnding}version: 2${lineEnding}---${lineEnding}\n<span class="alert alert-info">This is an alert</span>\n`;
+    var fixture = '---' + lineEnding + 'abc: xyz' + lineEnding + 'version: 2' + lineEnding + '---' + lineEnding + '\n<span class="alert alert-info">This is an alert</span>\n';
     var actual = matter(fixture);
     actual.should.have.property('data', {abc: 'xyz', version: 2});
     actual.should.have.property('content', '\n<span class="alert alert-info">This is an alert</span>\n');
@@ -44,43 +44,43 @@ describe(`Read from strings with lineEnding ${lineEnding.replace('\n', '\\n').re
   });
 
   it('should use a custom delimiter as a string.', function () {
-    var fixture = `~~~${lineEnding}abc: xyz${lineEnding}version: 2${lineEnding}~~~${lineEnding}\n<span class="alert alert-info">This is an alert</span>\n`;
+    var fixture = '~~~' + lineEnding + 'abc: xyz' + lineEnding + 'version: 2' + lineEnding + '~~~' + lineEnding + '\n<span class="alert alert-info">This is an alert</span>\n';
     var actual = matter(fixture, {delims: '~~~'});
     actual.should.have.property('data', {abc: 'xyz', version: 2});
-    actual.should.have.property('content', `\n<span class="alert alert-info">This is an alert</span>\n`);
+    actual.should.have.property('content', '\n<span class="alert alert-info">This is an alert</span>\n');
     actual.should.have.property('orig');
   });
 
   it('should use custom delimiters as an array.', function () {
-    var fixture = `~~~${lineEnding}abc: xyz${lineEnding}version: 2${lineEnding}~~~${lineEnding}${lineEnding}<span class="alert alert-info">This is an alert</span>${lineEnding}`;
+    var fixture = '~~~' + lineEnding + 'abc: xyz' + lineEnding + 'version: 2' + lineEnding + '~~~' + lineEnding + lineEnding + '<span class="alert alert-info">This is an alert</span>' + lineEnding;
     var actual = matter(fixture, {delims: ['~~~']});
     actual.should.have.property('data', {abc: 'xyz', version: 2});
-    actual.should.have.property('content', `${lineEnding}<span class="alert alert-info">This is an alert</span>${lineEnding}`);
+    actual.should.have.property('content', '' + lineEnding + '<span class="alert alert-info">This is an alert</span>' + lineEnding);
     actual.should.have.property('orig');
   });
 
   it('should correctly identify delimiters and ignore strings that look like delimiters.', function () {
-    var fixture = `---${lineEnding}name: "troublesome --- value"${lineEnding}---${lineEnding}here is some content${lineEnding}`;
+    var fixture = '---' + lineEnding + 'name: "troublesome --- value"' + lineEnding + '---' + lineEnding + 'here is some content' + lineEnding;
     var actual = matter(fixture);
     actual.should.have.property('data', {name: 'troublesome --- value'});
-    actual.should.have.property('content', `here is some content${lineEnding}`);
-    actual.should.have.property('orig', `---${lineEnding}name: "troublesome --- value"${lineEnding}---${lineEnding}here is some content${lineEnding}`);
+    actual.should.have.property('content', 'here is some content' + lineEnding);
+    actual.should.have.property('orig', '---' + lineEnding + 'name: "troublesome --- value"' + lineEnding + '---' + lineEnding + 'here is some content' + lineEnding);
   });
 
   it('should correctly parse a string that only has an opening delimiter.', function () {
-    var fixture = `---${lineEnding}name: "troublesome --- value"${lineEnding}`;
+    var fixture = '---' + lineEnding + 'name: "troublesome --- value"' + lineEnding;
     var actual = matter(fixture);
     actual.should.have.property('data', {name: 'troublesome --- value'});
     actual.should.have.property('content', '');
-    actual.should.have.property('orig', `---${lineEnding}name: "troublesome --- value"${lineEnding}`);
+    actual.should.have.property('orig', '---' + lineEnding + 'name: "troublesome --- value"' + lineEnding);
   });
 
   it('should not try to parse a string has content that looks like front-matter.', function () {
-    var fixture = `-----------name--------------value${lineEnding}foo`;
+    var fixture = '-----------name--------------value' + lineEnding + 'foo';
     var actual = matter(fixture);
     actual.should.have.property('data', {});
-    actual.should.have.property('content', `-----------name--------------value${lineEnding}foo`);
-    actual.should.have.property('orig', `-----------name--------------value${lineEnding}foo`);
+    actual.should.have.property('content', '-----------name--------------value' + lineEnding + 'foo');
+    actual.should.have.property('orig', '-----------name--------------value' + lineEnding + 'foo');
   });
 });
 
