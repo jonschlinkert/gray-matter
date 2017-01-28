97506af084aa0bd101f338caafe0cb0dadccc948
heymind <heymind@outlook.com>
2017-01-28 23:34:03 +0800

    modified:   gray-matter.d.ts

--- gray-matter.d.ts
+++ gray-matter.d.ts
@@ -1,16 +1,20 @@
-interface GrayMatterOption{
+declare module "gray-matter" {
+    export interface GrayMatterOption{
     parser?:Function;
     eval?:boolean;
     lang?:string;
     delims:string;
-}
-interface GrayMatter {
-    (str:string,options:GrayMatterOption,delims:Array<string>,
-    parser:Function):Object;
-    read(fp:string,options:GrayMatterOption):Object;
-    stringify(str:string,data:Object,options:GrayMatterOption):string;
+    }
+    export interface GrayMatter {
+        (str:string,options:GrayMatterOption,delims:Array<string>,
+        parser:Function):Object;
+        read(fp:string,options:GrayMatterOption):Object;
+        stringify(str:string,data:Object,options:GrayMatterOption):string;
+    }
+
+    var matter:GrayMatter;
+    export default matter;
+    
 }
 
-declare module "gray-matter" {
-    export = GrayMatter;
-}
+
