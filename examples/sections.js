f1d62fd334d20d734b23017129d7abef9ecc43a8
Lukas <lukasstrassel@googlemail.com>
2022-06-23 10:04:42 +0200

    feat: try updating to use yaml

--- examples/sections.js
+++ examples/sections.js
@@ -2,14 +2,14 @@
 
 const fs = require('fs');
 const path = require('path');
-const yaml = require('js-yaml');
+const { parse } = require('yaml');
 const matter = require('..');
 const str = fs.readFileSync(path.join(__dirname, 'fixtures', 'sections.md'));
 
 const file = matter(str, {
   section: function(section, file) {
     if (typeof section.data === 'string' && section.data.trim() !== '') {
-      section.data = yaml.safeLoad(section.data);
+      section.data = parse(section.data);
     }
     section.content = section.content.trim() + '\n';
   }
